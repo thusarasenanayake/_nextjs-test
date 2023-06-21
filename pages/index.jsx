48de96e1d4ee8d85b0c68c6c534fb7ad44258b4f
Thusara Senanayake <89896736+thusarasenanayake@users.noreply.github.com>
2023-06-21 23:28:09 +0530

    chore: add page with database operations

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,9 +4,11 @@ export default function index() {
       <p>/ssr</p>
       <p>/ssr-with-api</p>
       <p>/ssr-with-7s-delay</p>
+      <p>/ssr-with-mui</p>
 
       <p>/ssg</p>
       <p>/ssg-with-api</p>
+      <p>/ssr-with-database</p>
     </div>
   );
 }
